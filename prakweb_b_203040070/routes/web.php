d74b685de1b321f7f4c672d4d59d7226369d18da
Andip29 <priyandi.zembarazizi@gmail.com>
2022-10-30 01:25:17 +0700

    Menyelesaikan video pembelajaran 8 Category & Relationships

--- prakweb_b_203040070/routes/web.php
+++ prakweb_b_203040070/routes/web.php
@@ -4,6 +4,7 @@ use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
+use App\Models\Category;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,13 +19,13 @@ use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('home', [
-        "tittle" => "Home"
+        "title" => "Home"
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
-        "tittle" => "About",
+        "title" => "About",
         "name" => "Priyandi Zembar Azizi",
         "email" => "priyandi.zembarazizi@gmail.com",
         "image" => "gambar.jpg"
@@ -34,3 +35,18 @@ Route::get('/about', function () {
 
 Route::get('/posts', [PostController::class, 'index']);
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('/categories', function() {
+    return view('categories', [
+        'title' => 'Post Categories',
+        'categories' => category::all()
+    ]);
+});
+
+Route::get('/categories/{category:slug}', function(Category $category) {
+    return view('category', [
+        'title' => $category->name,
+        'posts' => $category->posts,
+        'category' => $category->name
+    ]);
+});
